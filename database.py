e4ccebdb768cce7e2b7a33fba58409ee57bb1fe4
hangnt <hangnt@datascience.com.vn>
2020-02-21 14:44:43 +0700

    [message ]

---
+++ database.py
@@ -0,0 +1,31 @@
+import sys
+import pymysql
+import logging
+"""Config values."""
+from os import environ
+class Database:
+    """Database connection class."""
+    def __init__(self, config):
+        self.host = config.db_host
+        self.username = config.db_user
+        self.password = config.db_password
+        self.port = config.db_port
+        self.dbname = config.db_name
+        self.conn = None
+    """Database connection class."""
+    def open_connection(self):
+        """Connect to MySQL Database."""
+        try:
+            if self.conn is None:
+                self.conn = pymysql.connect(self.host,
+                                            user=self.username,
+                                            passwd=self.password,
+                                            db=self.dbname,
+                                            connect_timeout=5)
+        except pymysql.MySQLError as e:
+            logging.error(e)
+            sys.exit()
+        finally:
+            logging.info('Connection opened successfully.')
+from info_database import Config 
+db = Database(Config)
